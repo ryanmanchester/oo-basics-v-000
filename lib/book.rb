9cba5d1a547787bd86e7580568c6b4d7e56676bf
Ryan Manchester <ryanm17@gmail.com>
2017-11-07 16:54:02 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,7 @@
 
 class Book
   attr_accessor :author, :page_count, :genre
-  
-  def initialize(title)
-    @title = title
-  end
-
-  def title
-    @title
-  end
+  attr_reader :title 
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
